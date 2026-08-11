da928c6003c0e67c8396303a7a38fd8d73ba6207
Shivprakash chavan <142031100+shivprakash9421@users.noreply.github.com>
2026-08-11 11:47:49 +0530

    Force key update and log it

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,16 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWBcZkm6XVp-UNCdjAFBIYDtTbbbLRB44", // Fixed the lowercase 'c' here!
+  apiKey: "AIzaSyCWBcZkm6XVp-UNCdjAFBIYDtTbbbLRB44",
   authDomain: "mobilityos-3c7e5.firebaseapp.com",
   projectId: "mobilityos-3c7e5",
   storageBucket: "mobilityos-3c7e5.firebasestorage.app",
   messagingSenderId: "887377458344",
   appId: "1:887377458344:web:d1e63cd991aa43a8b43e9d"
 };
+
+// THIS WILL PROVE EXACTLY WHAT KEY THE SITE IS USING
+console.log("FIREBASE KEY BEING USED:", firebaseConfig.apiKey);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
